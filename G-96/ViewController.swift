0bf17839f0d3aaf9164754be3f6c9ae8c977f2b5
Dilvir Singh <101398345@CLC330-03m.local>
2025-02-12 16:21:41 -0500

    First commit

---
+++ G-96/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  G-96
+//
+//  Created by Dilvir Singh on 2025-02-12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
